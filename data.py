690499a0d191cc199f2cecc3ba07b688c3f5d468
Anushka agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2021-10-31 20:30:39 +0530

    updated data.py

--- data.py
+++ data.py
@@ -5,6 +5,9 @@ import numpy as np
 import os
 import cv2
 import pandas as pd
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import torchvision.transforms.functional as fn
 
 class Ctdataset(Dataset):
     def __init__(self, path):
@@ -26,11 +29,25 @@ class Ctdataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join("2A_images", self.image[idx])
-        img= np.array(Image.open(img_path))
+        img= Image.open(img_path).convert("RGB")
+        img= img.resize((150, 150))
+        img= np.array(img)
         img= img.astype(float)
+        #img = fn.resize(img, size=[150, 150])
+        #img= img.resize(150, 150)
         label= self.labels[idx]
+        # transform = A.Compose(
+        # [
+        #     A.Resize(height=250, width=250),
+        #     ToTensorV2()
+        # ]
+        # )
+        # if self.transform is not None:
+        #     augumentations = self.transform(image= img)
+        #     image= augumentations["img"]
+
         #label= self.class_map[label]
-        return  label
+        return img, label
     
         
 
@@ -38,7 +55,9 @@ train_path = os.path.join("files", "test_COVIDx_CT-2A.txt")
 test_path = os.path.join("files", "train_COVIDx_CT-2A.txt")
 
 dataset = Ctdataset(train_path)
-train_loader = DataLoader(dataset= dataset, batch_size= 3, shuffle= True)
-for i in train_loader:
-    print(i)
+train_loader = DataLoader(dataset= dataset, batch_size= 10, shuffle= True)
+
+dataiter= iter(train_loader)
+images, labels = dataiter.next()
+print(len(images))
 
