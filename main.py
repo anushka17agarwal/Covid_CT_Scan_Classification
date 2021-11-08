1f4f224d71ad46f4c05ae9f51f5c7a4a0c9eef20
Anushka agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2021-11-08 18:12:07 +0530

    added model files and readme

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from tqdm import tqdm
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #Loading the data
 
-train_path = os.path.join("files", "test_COVIDx_CT-2A.txt")  
-test_path = os.path.join("files", "train_COVIDx_CT-2A.txt")
+train_path = os.path.join("files", "train_COVIDx_CT-2A.txt")  
+test_path = os.path.join("files", "test_COVIDx_CT-2A.txt")
 
 train_dataset = Ctdataset(train_path)
 test_dataset= Ctdataset(test_path)
@@ -22,15 +22,15 @@ test_loader= DataLoader(dataset= test_dataset, batch_size= 10, shuffle= True)
 # dataiter= iter(test_loader)
 # images, labels = dataiter.next()
 model= BConv()
-
-
+#print(len(train_dataset))
+#print(len(test_dataset))
 #starting with training
 model= BConv().to(device)
 optimizer=Adam(model.parameters(),lr=0.001,weight_decay=0.0001)
 loss_function=nn.CrossEntropyLoss().cuda()
-num_epochs=10
-dataiter= iter(train_loader)
-image, labels = dataiter.next()
+num_epochs=13
+# dataiter= iter(train_loader)
+# image, labels = dataiter.next()
 # print(image.shape)
 # image= image.reshape(10, 3, 150, 150)
 # print(image)
@@ -38,9 +38,11 @@ image, labels = dataiter.next()
 best_accuracy=0.0
 for epoch in tqdm(range(num_epochs)):
     model.train()
-    train_accuracy=0.0
-    train_loss=0.0
-    
+    running_loss= 0
+    correct = 0
+    total= 0
+    train_accu= []
+    train_losses= []
     for i, (images,labels) in enumerate(train_loader):
         images= images.permute(0,3,1,2)
         images= images.float()
@@ -58,38 +60,57 @@ for epoch in tqdm(range(num_epochs)):
         loss.backward()
         optimizer.step()
         
+        #print(images.size(0), images.size(1),images.size(2),"images.size(0)")
+        running_loss += loss.item()
+        _,predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+    
+    train_loss=running_loss/len(train_loader)
+    accu=100.*correct/total
+    accu=100.*correct/total
+    train_accu.append(accu)
+    train_losses.append(train_loss)
+    print('Train Loss: %.3f | Accuracy: %.3f'%(train_loss,accu))
+
+
+    
+      
+    model.eval()
+    
+    eval_losses=[]
+    eval_accu=[]
+    running_loss=0
+    correct=0
+    total=0
+
+    for i, (images,labels) in enumerate(test_loader):
+        images= images.permute(0,3,1,2)
+        images= images.float()
+        #print("converting image and labels for training")
+        images= images.to(device)
+        labels= labels.to(device)
+        outputs=model(images)
+ 
+        loss= loss_function(outputs,labels)
+        running_loss+=loss.item()
         
-        train_loss+= loss.cpu().data*images.size(0)
-        _,prediction=torch.max(outputs.data,1)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+            
+    test_loss=running_loss/len(test_loader)
+    accu=100.*correct/total
         
-        train_accuracy+=int(torch.sum(prediction==labels.data))
-        #print(train_accuracy, "after epoch")
+    eval_losses.append(test_loss)
+    eval_accu.append(accu)
         
-        train_accuracy=train_accuracy/len(train_loader)
-        print(train_accuracy, "train accuracy")
-        #train_loss=train_loss/train_count
+    print('Test Loss: %.3f | Accuracy: %.3f'%(test_loss,accu)) 
+
+
+
+
     
     
-        # Evaluation on testing dataset
-        model.eval()
-    
-        test_accuracy=0.0
-        for i, (images,labels) in enumerate(test_loader):
-            images= images.float()
-            images= images.permute(0,3,1,2)
-            images= images.to(device)
-            labels= labels.to(device)
-            outputs=model(images)
-            #images= images.float()
-            #_,prediction=torch.max(outputs.data,1)
-            #test_accuracy+=int(torch.sum(prediction==labels.data))
-        
-        test_accuracy=test_accuracy/len(test_loader)
-        
-        
-        print('Epoch: '+str(epoch)+' Train Loss: '+str(train_loss)+' Train Accuracy: '+str(train_accuracy)+' Test Accuracy: '+str(test_accuracy))
-        
-        #Save the best model
-        if test_accuracy>best_accuracy:
-            torch.save(model.state_dict(),'best_checkpoint.model')
-            best_accuracy=test_accuracy
+    torch.save(model.state_dict(),'best_checkpoint.model')
+  
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Covid CT Classification
+This is the implementation of a convolutional Neural Network on <a href= "https://www.kaggle.com/hgunraj/covidxct" >COVIDx CT  </a>
+
+## About the Dataset
+
+- The following dataset contains CT images from 194,922 CT slices from 3,745 patients and 201,103 CT slices from 4501 patients respectively.
+
+- Classes are zero-indexed with Normal=0, Pneumonia=1, and COVID-19=2
+- The COVIDx CT-2 dataset is released as a directory of images (2A_images) and associated label files ({train,val,test}_COVIDx_CT-2A.txt) indicating classes and bounding boxes for the body region.
+
+
+Link to download the dataset: <a href="https://www.kaggle.com/hgunraj/covidxct"> Here </a>
+
+
+## Downloading the trained Model:
+<a href="https://drive.google.com/file/d/1n-7OYvLqkg0E5VNBypnV5tDTAFSlH_o_/view?usp=sharing"> Here </a>
+
+
+## Accuracy Scores
+- Train Loss: 0.026 | Accuracy: 99.146
+- Test Loss: 1.190 | Accuracy: 76.404
+     
